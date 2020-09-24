332555f16f592d7f59b87633272c8da6a0d6f15b
Nykarri <karri.bonez@gmail.com>
2020-09-24 08:41:16 -0400

    added a cart scaffold -empty

--- store_api/db/migrate/20200924121201_create_products.rb
+++ store_api/db/migrate/20200924121201_create_products.rb
@@ -2,9 +2,11 @@ class CreateProducts < ActiveRecord::Migration[6.0]
   def change
     create_table :products do |t|
       t.string :name
-      t.float :price
+      t.decimal :price, precision: 5, scale: 2, default: 0
       t.string :description
       t.string :product_img
+      t.stirng :brand 
+  
 
       t.timestamps
     end
