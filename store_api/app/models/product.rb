a903b1b1db045bdb7bf236eb54ed490aca0eff28
Nykarri <karri.bonez@gmail.com>
2020-09-24 09:47:32 -0400

    updated products model to validate the presence of of required fields and relationships to line items and user

--- store_api/app/models/product.rb
+++ store_api/app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ApplicationRecord
+    belongs_to :user, optional: true
+    has_many :line_items
+
+    validates :name, :brand, :price, :description, :product_img, presence: true
 end
